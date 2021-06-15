a02e8e6088651996bff08860c82f649999652d41
Kane Bold <koboll1985@gmail.com>
2021-06-15 07:30:29 +0500

    HW02/base step 3. On end of workday.

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -3,14 +3,17 @@ package ru.javawebinar.topjava.repository.inmemory;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private final Map<Integer, MealDataBase> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
@@ -19,13 +22,14 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Meal save(Meal meal) {
+        MealDataBase dataBase = new MealDataBase(meal);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            repository.put(meal.getId(), dataBase);
             return meal;
         }
         // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        return Objects.requireNonNull(repository.computeIfPresent(meal.getId(), (id, old) -> dataBase)).meal;
     }
 
     @Override
@@ -35,11 +39,22 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Meal get(int id) {
-        return repository.get(id);
+        return repository.get(id).meal;
     }
 
     @Override
     public Collection<Meal> getAll() {
-        return repository.values();
+        return repository.values().stream()
+                .map(mealDataBase -> mealDataBase.meal)
+                .collect(Collectors.toList());
+    }
+
+    private static class MealDataBase {
+        private final int FOREIGN_KEY = SecurityUtil.authUserId();
+        private final Meal meal;
+
+        private MealDataBase(Meal meal) {
+            this.meal = meal;
+        }
     }
 }
