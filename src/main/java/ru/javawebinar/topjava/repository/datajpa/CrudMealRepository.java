4aa8d4478ec15d684f5f2bec38d4882726366f2b
Kane Bold <koboll1985@gmail.com>
2021-07-09 04:38:38 +0500

    HW05. Base. Step 1

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,28 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
-public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Transactional(readOnly = true)
+public interface CrudMealRepository extends  JpaRepository<Meal, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    int delete(@Param("id") int id, @Param("userId") int userid);
+
+    Meal findByIdAndUser_Id(int id, int userId);
+
+    List<Meal> findAllByUser_IdOrderByDateTimeDesc(int userId);
+
+    List<Meal> findAllByDateTimeIsGreaterThanEqualAndDateTimeBeforeAndUser_IdOrderByDateTimeDesc(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId);
+
 }
